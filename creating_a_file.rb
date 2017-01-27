4f22534cff27b6f69a025845631e6b407a452815
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-26 20:34:58 -0700

    first commit

---
+++ creating_a_file.rb
@@ -0,0 +1,7 @@
+File.open("files-lessons/teams.txt", 'W+') {|f| f.write("Twins, Astros, Mets, Yankees")}
+
+# W+ reading and writing
+# r - reading
+# w - just writing
+# a+ - open a file for reading and appending
+# r+ - opening a file for updating both reading and writing
